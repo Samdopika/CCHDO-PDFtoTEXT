428b91ebd9574306ff72c5ba3868031a7c6f3e0d
saman khadivar <91559399+Samdopika@users.noreply.github.com>
2023-09-18 12:41:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
-const express = require('express');
-const multer = require('multer');
-const pdf = require('pdf-parse');
+document.getElementById('uploadBtn').addEventListener('click', handlePDFUpload);
 
-const app = express();
-const port = 3000;
+async function handlePDFUpload() {
+    const fileInput = document.getElementById('pdfInput');
+    const file = fileInput.files[0];
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-app.use(express.static('public'));
-
-app.post('/upload', upload.single('pdf'), async (req, res) => {
-    try {
-        const extractedText = await extractTextFromPDF(req.file.buffer);
-
-        res.json({
-            success: true,
-            text: extractedText
-        });
-    } catch (error) {
-        console.error('Error:', error);
-        res.json({
-            success: false,
-            message: 'Failed to convert PDF'
-        });
+    if (!file) {
+        alert('Please select a PDF file before uploading.');
+        return;
     }
-});
 
-async function extractTextFromPDF(pdfBuffer) {
-    const data = await pdf(pdfBuffer);
-    return data.text;
+    convertPDFToText(file);
 }
 
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
-});
+async function convertPDFToText(file) {
+    const reader = new FileReader();
+
+    reader.onload = async (event) => {
+        const typedArray = new Uint8Array(event.target.result);
+        const pdfDocument = await pdfjsLib.getDocument(typedArray).promise;
+        let finalText = '';
+
+        for (let pageNum = 1; pageNum <= pdfDocument.numPages; pageNum++) {
+            const page = await pdfDocument.getPage(pageNum);
+            const content = await page.getTextContent();
+            finalText += content.items.map(item => item.str).join(' ') + '\n';
+        }
+
+        document.getElementById('outputText').value = finalText;
+    };
+
+    reader.readAsArrayBuffer(file);
+}
